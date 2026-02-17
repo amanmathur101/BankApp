899ae14aa82014da6174e919451db1b9c480faf2
amanmathur101 <121963646+amanmathur101@users.noreply.github.com>
2026-02-18 00:11:30 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://bankapp-dcft.onrender.com/api",
   withCredentials: true,
 });
 
